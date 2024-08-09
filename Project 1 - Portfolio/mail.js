c843a5ec30b94551351e8e34d5805ffa529893d7
Mahee Jaiswal <mahee0088@gmail.com>
2024-08-10 00:53:35 +0530

    Added More projects to my file

---
+++ Project 1 - Portfolio/mail.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYjNJO8x935GerdITtjqI0YzM2NIOZEaM",
+    authDomain: "portfolio-application-e17b6.firebaseapp.com",
+    databaseURL: "https://portfolio-application-e17b6-default-rtdb.firebaseio.com",
+    projectId: "portfolio-application-e17b6",
+    storageBucket: "portfolio-application-e17b6.appspot.com",
+    messagingSenderId: "849316268336",
+    appId: "1:849316268336:web:66f14ebfbe60129e61e0f5",
+    measurementId: "G-F8GCV2VZQK"
+  };
+
+  firebase.initializeApp(firebaseConfig);
---
+++ Project 4 - Random Password Generator/script.js
@@ -0,0 +1,28 @@
+const passswordBox = document.getElementById("password");
+const lengh = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "123456789";
+const symbol = "@#$%^&*()_+|}{[]<>/-=";
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password="";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+    
+    while(lengh>password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    
+    }
+    passswordBox.value = password;
+
+
+}
+
+function copyPassword() {
+    passswordBox.select();
+    document.execCommand("copy");
+}
